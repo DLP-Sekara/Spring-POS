2a7ddcf35945953d265739af286bd51c18d175cf
DLP-Sekara <dslahirupiyumal99@gmail.com>
2022-07-07 00:24:19 +0530

    added customer controller functions

--- src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,11 +1,47 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CustomerDto;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
+
+    @Autowired
+    //CustomerServiceImpl customerService;
+    CustomerService customerService;
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCustomer(){
+        //customerService.getAllCustomer();
+        return new ResponseUtil(200,"Done",customerService.getAllCustomer());
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDto customer){
+        customerService.saveCustomer(customer);
+        return new ResponseUtil(200,"Done",null);
+    }
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDto customer){
+        customerService.updateCustomer(customer);
+        return new ResponseUtil(200,"Done",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCustomer(@RequestParam String id){
+        customerService.deleteCustomer(id);
+        return new ResponseUtil(200,"Done",null);
+    }
+
+    @GetMapping(path="/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@PathVariable String id){
+        return new ResponseUtil(200,"Done",customerService.searchCustomer(id));
+    }
 }
--- src/main/java/lk/ijse/spring/controller/ItemController.java
+++ src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -1,11 +1,48 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CustomerDto;
+import lk.ijse.spring.dto.ItemDto;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.service.ItemService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("item")
 public class ItemController {
+    @Autowired
+    //CustomerServiceImpl customerService;
+    ItemService itemService;
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllItem(){
+        return new ResponseUtil(200,"Done",itemService.getAllItem());
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveItem(@ModelAttribute ItemDto item){
+        itemService.saveItem(item);
+        return new ResponseUtil(200,"Done",null);
+    }
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateItem(@RequestBody ItemDto item){
+        itemService.updateItem(item);
+        return new ResponseUtil(200,"Done",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteItem(@RequestParam String id){
+        itemService.deleteItem(id);
+        return new ResponseUtil(200,"Done",null);
+    }
+
+    @GetMapping(path="/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchItem(@PathVariable String id){
+        // customerService.searchCustomer(id);
+        return new ResponseUtil(200,"Done",itemService.searchItem(id));
+    }
 }
